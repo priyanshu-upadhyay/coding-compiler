4b89dcbcb0b13b11c6127e66cdb40b77ec2dff29
Priyanshu Upadhyay <priyanshu.upadh@gmail.com>
2023-04-23 17:36:58 +0000

    Added PM2

---
+++ prod-ecosystem.config.js
@@ -0,0 +1,36 @@
+const basePort = process.env.PM2_PORT || 3000;
+
+module.exports = {
+  apps: [
+    {
+      name: 'compiler-api-gateway',
+      script: 'dist/apps/api-gateway/main.js',
+      instances: 2,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: basePort + 0
+      },
+    },
+    {
+      name: 'compiler-backend-engine',
+      script: 'dist/apps/backend-engine/main.js',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: basePort + 1
+      },
+    },
+    {
+      name: 'compiler-cron-scheduler',
+      script: 'dist/apps/cron-scheduler/main.js',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: basePort + 2
+      },
+    },
+  ],
+};
